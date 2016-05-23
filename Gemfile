558e2e32658a63756cc897d6d0c10725268b3040
Pat Marshall <pmarshall1716@gmail.com>
2016-05-23 11:19:20 -0600

    casino we

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'https://rubygems.org'
+ruby '2.2.2'
+
+gem 'pry', '~>0.10.3'
+gem 'sinatra', '~>1.4.7'
---
+++ index.rb
@@ -0,0 +1,19 @@
+require 'sinatra'
+require 'pry'
+
+get '/casino' do
+	erb :main_menu
+end
+
+#slots call
+
+
+
+#hilow call
+
+
+
+#rock paper scissors call
+
+
+# not found page response
