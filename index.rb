e16870ddbf967c1da4dfa2a3ce96016435cc6350
Pat Marshall <pmarshall1716@gmail.com>
2016-05-23 16:17:00 -0600

    done

--- index.rb
+++ index.rb
@@ -2,10 +2,26 @@ require 'sinatra'
 require 'pry'
 
 get '/' do
-	erb :main_menu
-end
-
-get '/entrees' do
 	erb :entrees
 end
+
+get '/checkout' do
+	erb :checkout
+end
+
+get 'final_total' do
+	erb :final_total
+
+end
+
+post '/checkout' do 
+	@total = 0.00
+	@menu = {Cheeseburger: 4.00, Fries: 1.50, Hot_Dog: 3.00, Garden_Salad: 2.00, Pasta: 7.00, Mixed_Vegetables: 2.00, Chicken_Sandwhich: 6.00, Mashed_Taters: 3.00}
+	@selection = params.keys
+	@selection.each do |selection|
+		@total += @menu[selection.to_sym]
+	end 
+	erb :final_total
+end
+
 
